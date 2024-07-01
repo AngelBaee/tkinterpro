f8cb4d59a1f8aef3fcc3e971c943c8e0e6ee705b
Emily <140002969+AngelBaee@users.noreply.github.com>
2024-07-01 14:08:00 +0500

    first commit

---
+++ first.py
@@ -0,0 +1,11 @@
+import tkinter
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Very first Pro")
+window.geometry("300x200")
+label_name = tk.Label(text = "bla bla bla")
+label_name.grid(column = 0, row = 0)
+
+
+window.mainloop()
